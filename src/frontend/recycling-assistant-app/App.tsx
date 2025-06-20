7e9f8180524f9eb13aa1c759011a4c0305ee56d6
Mann Talati <mann.talati@gmail.com>
2025-06-19 22:42:51 -0500

    changes

--- src/frontend/recycling-assistant-app/App.tsx
+++ src/frontend/recycling-assistant-app/App.tsx
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { Amplify } from 'aws-amplify';
+//import { Storage } from '@aws-amplify/storage';
+//import awsExports from './aws-exports';
+
+//Amplify.configure(awsExports);
 
 export default function App() {
 
@@ -13,9 +18,11 @@ export default function App() {
   }
 
   async function upload() {
-    let result = ImagePicker.launchImageLibraryAsync();
-    if (!(await result).canceled) {
-      console.log('error');
+    const key = `uploads/${Date.now()}.jpg`;
+    let result = await ImagePicker.launchImageLibraryAsync();
+    if (!result.canceled) {
+      //await Storage.put(key, { result.assets[0]?.uri }, { contentType: 'image.jpeg' });
+      return key
     }
     //upload the image to s3 bucket and do the prediction
   }
